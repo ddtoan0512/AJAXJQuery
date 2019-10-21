e680722b65eaac4f94159525907892aacbdccda0
Toan Dang <ddtoan991205@gmail.com>
2019-10-21 16:19:41 +0700

    First Commit

---
+++ AJAXTable/AJAXTable/Models/EmployeeModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace AJAXTable.Models
+{
+    public class EmployeeModel
+    {
+        public int ID { get; set; }
+        public string Name { get; set; }
+        public double Salary { get; set; }
+    }
+}
